d63d36bb525600c38ef84d1d973bad55b5402f0a
petrostrak <pit.trak@gmail.com>
2022-07-09 16:24:37 +0300

    Create wallet

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+
+	"github.com/petrostrak/Blockchain-in-Go/wallet"
 )
 
 func init() {
@@ -10,20 +12,7 @@ func init() {
 }
 
 func main() {
-	myBlockChanAddress := "my_blockchain_address"
-	blockChain := NewBlockchain(myBlockChanAddress)
-	blockChain.Print()
-
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	blockChain.AddTransaction("C", "D", 2.0)
-	blockChain.AddTransaction("X", "Y", 3.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	fmt.Printf("my_blockchain_address %.1f\n", blockChain.CalculateTotalAmout("my_blockchain_address"))
-	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmout("C"))
-	fmt.Printf("D %.1f\n", blockChain.CalculateTotalAmout("D"))
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
 }
